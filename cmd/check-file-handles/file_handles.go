81dbc066a6d62f4ca177c84527b538a2d8e418d9
Matt Jones <mattjones@yieldbot.com>
2015-12-22 22:13:28 -0500

    debugging

--- cmd/check-file-handles/file_handles.go
+++ cmd/check-file-handles/file_handles.go
@@ -18,6 +18,8 @@ import (
 )
 
 // Calculate if the value is over a threshold
+//
+//
 func determineThreshold(limit float64, threshold float64, numFD float64) bool {
 	alarm := true
 	tLimit := threshold / float64(100) * limit
